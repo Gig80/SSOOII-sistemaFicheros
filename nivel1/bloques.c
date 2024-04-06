f8a2c6bc04ba6429694c5cb3b3152723b0ecb6db
ailaromag <105111997+ailaromag@users.noreply.github.com>
2024-04-06 17:27:28 +0200

    Update bloques.c

--- nivel1/bloques.c
+++ nivel1/bloques.c
@@ -1,9 +1,5 @@
 #include "bloques.h"
 
-int bmount(const char* camino);
-int bumount();
-int bwrite(unsigned int nbloque, const void *buf);
-int bread(unsigned int nbloque, void *buf);
 
 int descriptor;
 
